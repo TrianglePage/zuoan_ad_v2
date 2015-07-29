21850ef0c9b87fac12a87779825d2fed02489ea8
wk1990ok <wk1990ok@126.com>
2015-07-29 21:33:17 +0800

    remodify arch.

--- js/myjs.js
+++ js/myjs.js
@@ -15,7 +15,6 @@ var mySwiper = new Swiper ('.swiper-container', {
         //     alert("df");
         swiperAnimate(swiper);
         building_animate(swiper.activeIndex);
-        page2_white_bk_animation(swiper.activeIndex);
         page4_draw_chart(swiper.activeIndex);
     }
 })
@@ -44,57 +43,6 @@ function building_animate(index)
     }
 };
 
-function page2_white_bk_animation(index)
-{
-    if(index == 1)
-    {
-        setTimeout(function(){
-        $("#page2_white_bk").css({
-            "left": "26%",
-            "width": "60%",
-            "transition-property":"width 0.5s linear 0s, left 0.5s linear 0s",
-            "-webkit-transition":"width 0.5s linear 0s, left 0.5s linear 0s",
-            "-moz-transition":"width 0.5s linear 0s, left 0.5s linear 0s",
-            "-o-transition":"width 0.5s linear 0s, left 0.5s linear 0s"});
-        }, 4500);
-
-        setTimeout(function(){
-            $("#page2_white_flash").css({
-                "left": "86%",
-                "width":"10px",
-                "transition-property":"left 0.5s linear 0s",
-                "-webkit-transition":"left 0.5s linear 0s",
-                "-moz-transition":"left 0.5s linear 0s",
-                "-o-transition":"left 0.5s linear 0s"});
-        }, 5500);
-
-        setTimeout(function(){
-        $("#page2_white_flash").css({
-            "width":"0px"});
-        }, 6000);
-    }
-    else
-    {
-        $("#page2_white_bk").css({
-            "left": "56%",
-            "width": "0%",
-            "transition-property":"width 0s linear 0s, left 0s linear 0s",
-            "-webkit-transition":"width 0s linear 0s, left 0s linear 0s",
-            "-moz-transition":"width 0s linear 0s, left 0s linear 0s",
-            "-o-transition":"width 0s linear 0s, left 0s linear 0s"});
-
-        setTimeout(function(){
-            $("#page2_white_flash").css({
-                "left": "26%",
-                "width":"0",
-                "transition-property":"left 0s linear 0s",
-                "-webkit-transition":"left 0s linear 0s",
-                "-moz-transition":"left 0s linear 0s",
-                "-o-transition":"left 0s linear 0s"});
-        }, 1000);
-    }
-}
-
 function page4_draw_chart(index)
 {
     if(index == 3)
@@ -104,7 +52,7 @@ function page4_draw_chart(index)
     else
     {
         var i;
-        var cav = document.getElementById("page4_top_left");
+        var cav = document.getElementById("page4_top");
         var children = cav.childNodes;
         for(i=0;i<children.length;i++){
             cav.removeChild(children[i]);
@@ -130,7 +78,7 @@ function drawChart(x) {
             break;
     }
 
-    var chart = new CanvasJS.Chart("page4_top_left", {
+    var chart = new CanvasJS.Chart("page4_top", {
         theme: "theme2",//theme1
         backgroundColor: "rgba(34,34,34,0)",
         title:{
@@ -143,15 +91,14 @@ function drawChart(x) {
                 type: ctype,
                 //color: "rgba(2,34,21,.5)",
                 dataPoints: [
-                    { color:"rgba(28,34,21,1)", label: "attack",  y: 10  },
-                    { color:"rgba(1,40,120,1)", label: "defence", y: 15},
-                    { color:"rgba(21,3,77,1)", label: "speed", y: 25  },
-                    { color:"rgba(200,88,81,1)", label: "power",  y: 30  },
-                    { color:"rgba(5,123,164,1)", label: "magic",  y: 28  }
+                    { color:"rgba(28,34,21,1)",  label: "attack",	y: 10 },
+                    { color:"rgba(1,40,120,1)",  label: "defence",	y: 15 },
+                    { color:"rgba(21,3,77,1)",	 label: "speed",	y: 25 },
+                    { color:"rgba(200,88,81,1)", label: "power",	y: 30 },
+                    { color:"rgba(5,123,164,1)", label: "magic",	y: 28 }
                 ]
             }
         ]
     });
     chart.render();
-
 }
